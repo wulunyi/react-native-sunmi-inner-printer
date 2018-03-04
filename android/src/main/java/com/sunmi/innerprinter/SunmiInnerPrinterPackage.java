873ccba8fb9f0621bd1352928c725ece01459cc7
吴伦毅 <wulunyi@weidian.com>
2018-03-04 18:07:43 +0800

    调试

--- android/src/main/java/com/sunmi/innerprinter/SunmiInnerPrinterPackage.java
+++ android/src/main/java/com/sunmi/innerprinter/SunmiInnerPrinterPackage.java
@@ -12,10 +12,10 @@ import java.util.List;
 
 public class SunmiInnerPrinterPackage implements ReactPackage {
 
-  @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
+  // @Override
+  // public List<Class<? extends JavaScriptModule>> createJSModules() {
+  //   return Collections.emptyList();
+  // }
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
